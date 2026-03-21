6440580aa7950b32ca975abe6a60f51c25dc0ca9
JBRprojects-37 <marcio.mclaren37@gmail.com>
2026-03-21 12:38:57 +0000

    AULA 5.0.

---
+++ vetores/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include <iostrream>
+
+using namespace std;
+
+int main(void){
+    int n = 3;
+    int vec[n] = (0, 1, 2);
+]   for(int i =0; i < n; i++){
+    cout << vec [i] << "\n";
+}
+    cout << i << "\n";
+
+    return 0;
+}
